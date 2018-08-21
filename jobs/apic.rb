0eeb088c500dab52e6ca7e33c3e8ef6404ee7d87
Chris McHenry <chmchenr@cisco.com>
2018-08-21 15:41:13 -0400

    Added Cisco sample code license

--- jobs/apic.rb
+++ jobs/apic.rb
@@ -1,7 +1,22 @@
+#Copyright (c) 2018 Cisco and/or its affiliates.
+#
+#This software is licensed to you under the terms of the Cisco Sample
+#Code License, Version 1.0 (the "License"). You may obtain a copy of the
+#License at
+#
+#               https://developer.cisco.com/docs/licenses
+#
+#All use of the material herein must be in accordance with the terms of
+#the License. All rights not expressly granted by the License are
+#reserved. Unless required by applicable law or agreed to separately in
+#writing, software distributed under the License is distributed on an "AS
+#IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+#or implied.
+
 require 'acirb'
 require 'time'
 
-apicuri = 'https://APIC-IP'
+apicuri = 'https://apic'
 username = 'admin'
 password = 'password'
 
